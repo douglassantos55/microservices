662b6ccdf342cc25bbe468baa5e1d02f6e71d2a2
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-01-05 17:02:41 -0300

    chore: fix proto compiling to generate client and server code

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-rm -f $1/proto/$2.pb.go
-protoc --go_out=. --go_opt=paths=source_relative $1/proto/$2.proto
+rm -f $1/proto/$2.pb.go \
+    $1/proto/$2_grpc.pb.go
+
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    $1/proto/$2.proto
